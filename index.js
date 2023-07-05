e35bda08479dda904626dca11201004bb26eb85c
David <david.ndiba@student.moringaschool.com>
2023-07-05 10:01:09 +0300

    almost done

---
+++ index.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", initialize);
+
+function initialize(){
+    fetchFromServer();
+    console.log("buy ticket");
+}
+
+function fetchFromServer(){
+    fetch("")
+}
